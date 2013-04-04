6827ee049edb19a933ec912ca3ce4349f561b596
resteve <resteve@zikzakmedia.com>
2013-04-04 15:41:12 +0200

    Remove views test

--- tests/test_party_search.py
+++ tests/test_party_search.py
@@ -23,12 +23,6 @@ class PartySearchTestCase(unittest.TestCase):
     def setUp(self):
         trytond.tests.test_tryton.install_module('party_search')
 
-    def test0005views(self):
-        '''
-        Test views.
-        '''
-        test_view('party_search')
-
     def test0006depends(self):
         '''
         Test depends.
