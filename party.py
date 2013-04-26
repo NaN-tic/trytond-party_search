cf3d7f1acb52a73559e54a20c6539050cacbc2aa
resteve <resteve@zikzakmedia.com>
2013-04-26 13:45:54 +0200

    pyflakes fixed

--- party.py
+++ party.py
@@ -1,10 +1,8 @@
 #This file is part party_search module for Tryton.
 #The COPYRIGHT file at the top level of this repository contains 
 #the full copyright notices and license terms.
-
-from trytond.model import fields
 from trytond.pool import Pool, PoolMeta
-from trytond.transaction import Transaction
+
 
 __all__ = ['Party']
 __metaclass__ = PoolMeta
