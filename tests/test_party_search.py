553ded61b2dddcb2db4bb75d1bf2aeac32d2db66
resteve <resteve@zikzakmedia.com>
2015-11-29 22:38:14 +0100

    Tests

--- tests/test_party_search.py
+++ tests/test_party_search.py
@@ -1,24 +1,14 @@
-#!/usr/bin/env python
-#This file is part party_search module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains 
-#the full copyright notices and license terms.
+# This file is part of the party_search module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
-import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
-from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class PartySearchTestCase(unittest.TestCase):
-    '''Test PartySearch module'''
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('party_search')
-
-    def test0006depends(self):
-        '''
-        Test depends'''
-        test_depends()
+class PartySearchTestCase(ModuleTestCase):
+    'Test Party Search module'
+    module = 'party_search'
 
 
 def suite():
