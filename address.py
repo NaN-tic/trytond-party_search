b3d5e5f6a01b3c1cf09c72d85ea06fdde0b22803
Raimon Esteve <raimon@nan-tic.com>
2018-09-15 18:10:29 +0200

    Migrate to py3

--- address.py
+++ address.py
@@ -6,8 +6,7 @@ from trytond.pool import PoolMeta
 __all__ = ['Address']
 
 
-class Address:
-    __metaclass__ = PoolMeta
+class Address(metaclass=PoolMeta):
     __name__ = 'party.address'
 
     @classmethod
